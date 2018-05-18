f174255f7e5944dc63da1769a3aab562cabb2e1f
Harsh Vardhan <harshv47@gmail.com>
2018-05-18 23:27:03 +0530

    Update main-single-hotword.py

--- main-single-hotword.py
+++ main-single-hotword.py
@@ -14,12 +14,8 @@ def interrupt_callback():
     global interrupted
     return interrupted
 
-if len(sys.argv) == 1:
-    print("Error: need to specify model name")
-    print("Usage: python demo.py your.model")
-    sys.exit(-1)
 
-model = sys.argv[1]
+model = "/models/hey_bot.pmdl
 
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
